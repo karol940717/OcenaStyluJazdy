9bfb2f7ef8209465751d2e557359752f597ee6f8
karol940717 <karol.chotkiewicz94@gmail.com>
2022-05-08 20:27:24 +0200

    .

---
+++ app/src/main/java/com/example/ocenastylujazdy/MainApplication.java
@@ -0,0 +1,16 @@
+package com.example.ocenastylujazdy;
+
+import android.app.Application;
+import android.content.Context;
+//klasa potrzebna do ustawienia języka aplikacji
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleHelper.onAttach(base, "en"));
+    }
+}
